eff8d606c3b0122f271d74cfa8366926fc8cb040
alvin.poh <alvin.poh@activate.sg>
2025-10-18 23:03:08 +0800

    [Alvin] Update Review model in accordance to the Review class diagram in GBA Q2.

--- app/models/review.py
+++ app/models/review.py
@@ -14,16 +14,46 @@ class Review(db.Document):
     rating = db.IntField(min_value=1, max_value=5, required=True)
     title = db.StringField(max_length=100)
     comment = db.StringField(max_length=1000)
-    review_date = db.DateTimeField(default=datetime.utcnow)
+    date = db.DateTimeField(default=datetime.utcnow)
+    suggested_theme = db.StringField(max_length=100)
+    image_url = db.StringField(max_length=1000)
+    
+    @staticmethod
+    def getAllReviews():
+        """Get all reviews from the database"""
+        return Review.objects()
+    
+    @staticmethod
+    def getReviewByPackage(package):
+        """Get all reviews by package"""
+        package = Package.getPackage(package)
+        if package:
+            return Review.objects(package=package)
+        return []
+    
+    @staticmethod
+    def getPackageAverageRating(package):
+        """Get the average rating of a package"""
+        reviews = Review.getReviewByPackage(package)
+        if reviews:
+            return sum(review.rating for review in reviews) / len(reviews)
+        return 0
+    
+    @staticmethod
+    def getReviewsByCustomer(customer):
+        """Get all reviews by customer"""
+        customer = User.getUser(customer)
+        if customer:
+            return Review.objects(customer=customer)
+        return []
+    
+        @staticmethod
+    def getReview(customer, package):
+        """Get a specific review by customer and package"""
+        return Review.objects(Q(customer=customer) & Q(package=package)).first()
 
     @staticmethod
-    def verify_review(self):
-        """Check if the reviewer has actually booked this package"""
-        booking_exists = Booking.objects(Q(customer=self.customer) & Q(package=self.package)).first()
-        return booking_exists
-
-    @staticmethod
-    def setReview(customer, package, booking, rating, title, comment):
+    def createReview(customer, package, booking, rating, title, comment):
         """Create and save a new review"""
         if Review.verify_review(Review(customer=customer, booking=booking, package=package)) is None:
             raise ValueError("Customer has not booked this package.")
@@ -40,6 +70,50 @@ class Review(db.Document):
         return new_review
 
     @staticmethod
-    def getReview(customer, package):
-        """Get a specific review by customer and package"""
-        return Review.objects(Q(customer=customer) & Q(package=package)).first()
+    def updateReview(customer, package, new_date, new_rating, new_comment, new_image_url, new_suggested_theme, new_title ):
+        """Update a review"""
+        review = Review.getReview(customer, package)
+        if review:
+            review.date = new_date
+            review.rating = new_rating
+            review.title = new_title
+            review.comment = new_comment
+            review.image_url = new_image_url
+            review.suggested_theme = new_suggested_theme
+            review.save()
+            return review
+        return None
+    
+    @staticmethod
+    def deleteReview(customer, package):
+        """Delete a review"""
+        review = Review.getReview(customer, package)
+        if review:
+            review.delete()
+            return True
+        return False
+
+    @staticmethod
+    def verify_review(self):
+        """Check if the reviewer has actually booked this package"""
+        booking_exists = Booking.objects(Q(customer=self.customer) & Q(package=self.package)).first()
+        return booking_exists
+    
+    # For the API branch to return JSON data
+    @staticmethod
+    def dereferenceReview(review):
+        return {
+            'date': review.date,
+            'customer': review.customer.email,
+            'package': review.package.hotel_name,
+            'rating': review.rating,
+            'title': review.title,
+            'comment': review.comment,
+            'image_url': review.image_url,
+            'suggested_theme': review.suggested_theme
+        }
+    
+    @staticmethod
+    def dereferenceReviews(reviews):
+        return [Review.dereferenceReview(review) for review in reviews]
+    
